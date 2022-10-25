e2761780f9e8293b4c8458cc37188380f9e70ec7
Klaus Mikaelson9 <dvalitov818@gmail.com>
2022-10-25 21:47:51 +0300

    les1

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.querySelector('.main__btn').addEventListener('click', function () {
+    document.querySelector('.main__list').classList.toggle('active');
+});
+document.querySelector('.main__btn').addEventListener('click', function () {
+    document.querySelector('.main__btn-svg').classList.toggle('svg__active');
+});
